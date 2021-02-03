8c447e2f4ec4f107329b1a85e2be129006368604
Peter <peter279k@gmail.com>
2021-02-03 17:51:03 +0800

    Create Non-TLS client/server examples

---
+++ examples/no_tls_server_example/tls_server_example.c
@@ -0,0 +1,215 @@
+/*
+ *  no_tls_server_example.c
+ *
+ *  How to configure a no TLS server
+ */
+
+#include "iec61850_server.h"
+#include "hal_thread.h"
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "static_model.h"
+
+/* import IEC 61850 device model created from SCL-File */
+extern IedModel iedModel;
+
+static int running = 0;
+static IedServer iedServer = NULL;
+
+void
+sigint_handler(int signalId)
+{
+    running = 0;
+}
+
+static MmsDataAccessError
+writeAccessHandler (DataAttribute* dataAttribute, MmsValue* value, ClientConnection connection, void* parameter)
+{
+    if (dataAttribute == IEDMODEL_GenericIO_GGIO1_NamPlt_vendor) {
+        char* newValue = MmsValue_toString(value);
+        printf("New value for OutVarSet_setMag_f = %s\n", newValue);
+        return DATA_ACCESS_ERROR_SUCCESS;
+    }
+
+    return DATA_ACCESS_ERROR_OBJECT_ACCESS_DENIED;
+}
+
+static ControlHandlerResult
+controlHandlerForBinaryOutput(ControlAction action, void* parameter, MmsValue* value, bool test)
+{
+    if (test)
+        return CONTROL_RESULT_FAILED;
+
+    if (MmsValue_getType(value) == MMS_BOOLEAN) {
+        printf("received binary control command: ");
+
+        if (MmsValue_getBoolean(value))
+            printf("on\n");
+        else
+            printf("off\n");
+    }
+    else
+        return CONTROL_RESULT_FAILED;
+
+    uint64_t timeStamp = Hal_getTimeInMs();
+
+    if (parameter == IEDMODEL_GenericIO_GGIO1_SPCSO1) {
+        IedServer_updateUTCTimeAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_SPCSO1_t, timeStamp);
+        IedServer_updateAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_SPCSO1_stVal, value);
+    }
+
+    if (parameter == IEDMODEL_GenericIO_GGIO1_SPCSO2) {
+        IedServer_updateUTCTimeAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_SPCSO2_t, timeStamp);
+        IedServer_updateAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_SPCSO2_stVal, value);
+    }
+
+    if (parameter == IEDMODEL_GenericIO_GGIO1_SPCSO3) {
+        IedServer_updateUTCTimeAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_SPCSO3_t, timeStamp);
+        IedServer_updateAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_SPCSO3_stVal, value);
+    }
+
+    if (parameter == IEDMODEL_GenericIO_GGIO1_SPCSO4) {
+        IedServer_updateUTCTimeAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_SPCSO4_t, timeStamp);
+        IedServer_updateAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_SPCSO4_stVal, value);
+    }
+
+    return CONTROL_RESULT_OK;
+}
+
+static void
+connectionHandler (IedServer self, ClientConnection connection, bool connected, void* parameter)
+{
+    if (connected)
+        printf("Connection opened\n");
+    else
+        printf("Connection closed\n");
+}
+
+static void
+printAppTitle(ItuObjectIdentifier* oid)
+{
+    int i;
+
+    for (i = 0; i < oid->arcCount; i++) {
+        printf("%i", oid->arc[i]);
+
+        if (i != (oid->arcCount - 1))
+            printf(".");
+    }
+}
+
+static bool
+clientAuthenticator(void* parameter, AcseAuthenticationParameter authParameter, void** securityToken, IsoApplicationReference* appRef)
+{
+    printf("ACSE Authenticator:\n");
+    printf("  client ap-title: "); printAppTitle(&(appRef->apTitle)); printf("\n");
+    printf("  client ae-qualifier: %i\n", appRef->aeQualifier);
+    printf("  auth-mechanism: %i\n", authParameter->mechanism);
+
+    return true;
+}
+
+int
+main(int argc, char** argv)
+{
+    int port_number = 8102;
+    if (argc > 1)
+        port_number = atoi(argv[1]);
+
+    printf("Using libIEC61850 version %s\n", LibIEC61850_getVersionString());
+    printf("libIEC61850 IedServer server will listen on %d\n", port_number);
+
+    // Create Model IED
+    IedModel* myModel = IedModel_create("testmodel");
+
+    // Create Logical Device
+    LogicalDevice* lDevice1 = LogicalDevice_create("SENSORS", myModel);
+
+    // Create Logical Node
+    LogicalNode* ttmp1 = LogicalNode_create("TTMP1", lDevice1);
+
+    // Create Model Node
+    CDC_ASG_create("TmpSp", (ModelNode*) ttmp1, 0, false);
+    CDC_VSG_create("TmpSt", (ModelNode*) ttmp1, 0);
+
+    // Create Data Object
+    DataObject* do1 = DataObject_create("Temp1", (ModelNode*) ttmp1, 0);
+
+    // Create Data Attribute
+    DataAttribute* fl = DataAttribute_create("float", (ModelNode*) do1, IEC61850_FLOAT64, IEC61850_FC_MX, 0, 0, 0);
+    DataAttribute* st = DataAttribute_create("string", (ModelNode*) do1, IEC61850_VISIBLE_STRING_255, IEC61850_FC_DC,0, 0, 0);
+
+    // Create Server Connection
+    iedServer = IedServer_create(&iedModel);
+
+    // Install writer handler
+    IedServer_handleWriteAccess(iedServer, IEDMODEL_GenericIO_GGIO1_NamPlt_vendor, writeAccessHandler, NULL);
+
+    IedServer_setAuthenticator(iedServer, clientAuthenticator, NULL);
+
+    IedServer_setWriteAccessPolicy(iedServer, IEC61850_FC_DC, ACCESS_POLICY_ALLOW);
+
+    /* MMS server will be instructed to start listening to client connections. */
+    IedServer_start(iedServer, port_number);
+
+    if (!IedServer_isRunning(iedServer)) {
+        printf("Starting server failed! Exit.\n");
+        IedServer_destroy(iedServer);
+        exit(-1);
+    }
+
+    running = 1;
+
+    signal(SIGINT, sigint_handler);
+
+    float t = 0.f;
+
+    while (running) {
+        uint64_t timestamp = Hal_getTimeInMs();
+
+        t += 0.1f;
+
+        float an1 = 0.0001;
+        float an2 = sinf(t + 1.f);
+        float an3 = sinf(t + 2.f);
+        float an4 = sinf(t + 3.f);
+
+        IedServer_lockDataModel(iedServer);
+
+        Timestamp iecTimestamp;
+
+        Timestamp_clearFlags(&iecTimestamp);
+        Timestamp_setTimeInMilliseconds(&iecTimestamp, timestamp);
+        Timestamp_setLeapSecondKnown(&iecTimestamp, true);
+
+        /* toggle clock-not-synchronized flag in timestamp */
+        if (((int) t % 2) == 0)
+            Timestamp_setClockNotSynchronized(&iecTimestamp, true);
+
+        IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn1_t, &iecTimestamp);
+        IedServer_updateFloatAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn1_mag_f, an1);
+
+        IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn2_t, &iecTimestamp);
+        IedServer_updateFloatAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn2_mag_f, an2);
+
+        IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn3_t, &iecTimestamp);
+        IedServer_updateFloatAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn3_mag_f, an3);
+
+        IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn4_t, &iecTimestamp);
+        IedServer_updateFloatAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn4_mag_f, an4);
+
+        IedServer_unlockDataModel(iedServer);
+
+        Thread_sleep(100);
+    }
+
+    /* stop MMS server - close TCP server socket and all client sockets */
+    IedServer_stop(iedServer);
+
+    /* Cleanup - free all resources */
+    IedServer_destroy(iedServer);
+
+} /* main() */
