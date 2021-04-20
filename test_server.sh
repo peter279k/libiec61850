4b5dd9a0c35bb46726f69c03bca4ed7357a5fdf9
Peter <peter279k@gmail.com>
2021-04-20 14:06:47 +0800

    Add write_data_attr argumment

--- test_server.sh
+++ test_server.sh
@@ -7,8 +7,14 @@ rest_color='\e[0m'
 
 mode=$1
 ip=$2
+write_data_attr=$3
 
-docker exec "$mode-iec61850-server" bash -c "cd no_tls_client_example/ && ./no_tls_client_example $ip 8102 test_string2"
+if [[ $write_data_attr == "" ]]; then
+    echo -e "${red_color}Please fill the data attribute string${rest_color}"
+    exit 1;
+fi;
+
+docker exec "$mode-iec61850-server" bash -c "cd no_tls_client_example/ && ./no_tls_client_example $ip 8102 $write_data_attr"
 
 if [[ $? != 0 ]]; then
     echo -e "${red_color}The $mode-iec61850-server container is failed to run.${rest_color}"
