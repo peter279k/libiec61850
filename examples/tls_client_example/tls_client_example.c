b67d628b7c71df1cfa79885a59fe0543b868d6a0
Chun-Sheng, Li <peter279k@gmail.com>
2021-05-04 16:28:01 +0800

    Resolved by issue #46 (#56)

--- examples/tls_client_example/tls_client_example.c
+++ examples/tls_client_example/tls_client_example.c
@@ -44,8 +44,8 @@ int create_sqlite3_table() {
         return 1;
     }
 
-    char *sql = "CREATE TABLE IF NOT EXISTS reading_value(id INTEGER PRIMARY KEY AUTOINCREMENT, value TEXT, date_time TEXT);"
-                "CREATE TABLE IF NOT EXISTS writing_data_attribute(id INTEGER PRIMARY KEY AUTOINCREMENT, attribute TEXT, date_time TEXT);";
+    char *sql = "CREATE TABLE IF NOT EXISTS inverter_info(id INTEGER PRIMARY KEY AUTOINCREMENT, watt TEXT, volt TEXT, electric TEXT, measured_date_time TEXT);"
+                "CREATE TABLE IF NOT EXISTS writing_attribute_log(id INTEGER PRIMARY KEY AUTOINCREMENT, attribute TEXT, created_date_time TEXT);";
     rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
     if (rc != SQLITE_OK) {
         fprintf(stderr, "SQL error: %s\n", err_msg);
@@ -59,7 +59,7 @@ int create_sqlite3_table() {
     return 0;
 }
 
-int insert_reading_value(char *insert_reading_sql) {
+int insert_inverter_value(char *insert_reading_sql) {
     sqlite3 *db;
     char *err_msg = 0;
     int rc = sqlite3_open_v2("/home/iec61850/databases/tls_libiec61850.db", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
@@ -115,6 +115,9 @@ char* get_current_datetime() {
     struct tm *local = localtime(&now);
     hours = local->tm_hour;
     hours = hours - 12;
+    if (hours < 0) {
+        hours += 12;
+    }
     minutes = local->tm_min;
     seconds = local->tm_sec;
 
@@ -188,22 +191,54 @@ int main(int argc, char** argv) {
         if (serverDirectory)
             LinkedList_destroy(serverDirectory);
 
-        /* read an analog measurement value from server */
+        /* read an analog measurement watt value from server */
         MmsValue* value = IedConnection_readObject(con, &error, "simpleIOGenericIO/GGIO1.AnIn1.mag.f", IEC61850_FC_MX);
 
+        char watt_fval_str[100];
+        char volt_fval_str[100];
+        char electric_fval_str[100];
+
+
         if (value != NULL) {
-            float fval = MmsValue_toFloat(value);
-            printf("read float value: %f\n", fval);
+            float watt_val = MmsValue_toFloat(value);
+            printf("read watt float value: %0.1f\n", watt_val);
             printf("Try to store reading value...\n");
             printf("Today Date time is: %s\n", get_current_datetime());
 
-            char *insert_reading_sql;
-            char fval_str[100];
-            gcvt(fval, 6, fval_str);
-            asprintf(&insert_reading_sql, "INSERT INTO reading_value(value, date_time) VALUES('%s', '%s');", fval_str, get_current_datetime());
-            insert_reading_value(insert_reading_sql);
+            gcvt(watt_val, 6, watt_fval_str);
             MmsValue_delete(value);
         }
+ 
+        /* read an analog measurement volt value from server */
+        value = IedConnection_readObject(con, &error, "simpleIOGenericIO/GGIO1.AnIn2.mag.f", IEC61850_FC_MX);
+
+        if (value != NULL) {
+            float volt_val = MmsValue_toFloat(value);
+            printf("read volt float value: %0.1f\n", volt_val);
+            printf("Try to store reading value...\n");
+            printf("Today Date time is: %s\n", get_current_datetime());
+
+            gcvt(volt_val, 6, volt_fval_str);
+            MmsValue_delete(value);
+        }
+
+        /* read an analog measurement electric value from server */
+        value = IedConnection_readObject(con, &error, "simpleIOGenericIO/GGIO1.AnIn3.mag.f", IEC61850_FC_MX);
+
+        if (value != NULL) {
+            float electric_val = MmsValue_toFloat(value);
+            printf("read electric float value: %0.1f\n", electric_val);
+            printf("Try to store reading value...\n");
+            printf("Today Date time is: %s\n", get_current_datetime());
+
+            gcvt(electric_val, 6, electric_fval_str);
+            MmsValue_delete(value);
+        }
+
+        /* Insert watt, volt and electric values to inverter_info */
+        char *insert_reading_sql;
+        asprintf(&insert_reading_sql, "INSERT INTO inverter_info(watt, volt, electric, measured_date_time) VALUES('%s', '%s', '%s', '%s');", watt_fval_str, volt_fval_str, electric_fval_str, get_current_datetime());
+        insert_inverter_value(insert_reading_sql);
 
         /* write a variable to the server */
         value = MmsValue_newVisibleString(attribute_string);
@@ -216,7 +251,7 @@ int main(int argc, char** argv) {
             printf("Writing data attribute to server has been successful!\n");
             printf("Trying to insert data attribute to SQLite writing_data_attribute table...\n");
             char *insert_attr_sql;
-            asprintf(&insert_attr_sql, "INSERT INTO writing_data_attribute(attribute, date_time) VALUES('%s', '%s');", attribute_string, get_current_datetime());
+            asprintf(&insert_attr_sql, "INSERT INTO writing_attribute_log(attribute, created_date_time) VALUES('%s', '%s');", attribute_string, get_current_datetime());
             insert_writing_attr(insert_attr_sql);
         }
 
--- examples/tls_server_example/tls_server_example.c
+++ examples/tls_server_example/tls_server_example.c
@@ -10,14 +10,280 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <curl/curl.h>
+#include <json-c/json.h>
 
 #include "static_model.h"
+
+#define MAX_CHAR_SIZE 300
+
+static char INVERTER_GET_INFO[MAX_CHAR_SIZE];
+static char INVERTER_GET_STATUS[MAX_CHAR_SIZE];
+static char INVERTER_SET[MAX_CHAR_SIZE];
+
+static char INVERTER_INFO_RES[MAX_CHAR_SIZE];
+static char INVERTER_STATUS_RES[MAX_CHAR_SIZE];
+static char INVERTER_SET_RES[MAX_CHAR_SIZE];
+
+static float INVERTER_CURRENT;
+static float INVERTER_VOLT;
+static float INVERTER_POWER;
+
+static bool INVERTER_ON_OFF;
+static bool INVERTER_CONNECTION;
+static char INVERTER_VERSION[MAX_CHAR_SIZE];
+static char INVERTER_CONNECTION_STATUS[MAX_CHAR_SIZE];
+
+static char INVERTER_ON_STRING[MAX_CHAR_SIZE] = "inverter_on";
+static char INVERTER_OFF_STRING[MAX_CHAR_SIZE] = "inverter_off";
+
+struct memory_struct {
+    char *memory;
+    size_t size;
+};
 
 /* import IEC 61850 device model created from SCL-File */
 extern IedModel iedModel;
 
 static int running = 0;
 static IedServer iedServer = NULL;
+
+static size_t receive_callback(void *contents, size_t size, size_t nmemb, void *userp) {
+    size_t realsize = size * nmemb;
+    struct memory_struct *mem = (struct memory_struct *)userp;
+
+    char *ptr = realloc(mem->memory, mem->size + realsize + 1);
+    if(ptr == NULL) {
+        /* out of memory! */
+        fprintf(stderr, "receive_callback(): not enough memory!\n");
+        return 0;
+    }
+
+    mem->memory = ptr;
+    memcpy(&(mem->memory[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
+
+    return realsize;
+}
+
+void process_response_json(char *res_json) {
+    struct json_object *obj;
+    struct json_object *value;
+    enum json_tokener_error error;
+    obj = json_tokener_parse_verbose(res_json, &error);
+    if (error != json_tokener_success) {
+        fprintf(stderr, "process_response_json: %s parsing is failed.\n", res_json);
+    } else {
+        bool is_found = json_object_object_get_ex(obj, "success", &value);
+        if (is_found) {
+            INVERTER_ON_OFF = json_object_get_boolean(value);
+            fflush(stdout);
+        }
+        is_found = json_object_object_get_ex(obj, "LoadCurrent", &value);
+
+        if (is_found) {
+            INVERTER_CURRENT = json_object_get_int(value) * 0.01;
+        }
+        is_found = json_object_object_get_ex(obj, "LoadVolt", &value);
+
+        if (is_found) {
+            INVERTER_VOLT = json_object_get_int(value) * 0.1;
+        }
+        is_found = json_object_object_get_ex(obj, "LoadPower", &value);
+
+        if (is_found) {
+            INVERTER_POWER = json_object_get_int(value) * 0.1;
+        }
+        is_found = json_object_object_get_ex(obj, "version", &value);
+
+        if (is_found) {
+            strcpy(INVERTER_VERSION, "");
+            strcpy(INVERTER_VERSION, json_object_get_string(value));
+        }
+        is_found = json_object_object_get_ex(obj, "connStatus", &value);
+
+        if (is_found) {
+            strcpy(INVERTER_CONNECTION_STATUS, "");
+            strcpy(INVERTER_CONNECTION_STATUS, json_object_get_string(value));
+        }
+        is_found = json_object_object_get_ex(obj, "connection", &value);
+
+        if (is_found) {
+            INVERTER_CONNECTION = json_object_get_boolean(value);
+        }
+        is_found = json_object_object_get_ex(obj, "InverterOn", &value);
+
+        if (is_found) {
+            INVERTER_ON_OFF = json_object_get_boolean(value);
+        }
+    }
+}
+
+void fetch_inverter_info() {
+    CURL *curl = curl_easy_init();
+    CURLcode res;
+    struct memory_struct chunk;
+
+    chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */ 
+    chunk.size = 0;    /* no data at this point */ 
+
+    if (!curl) {
+        fprintf(stderr, "libcurl is not loaded correctly!\n");
+    }
+
+    // set cURL setting
+    curl_easy_setopt(curl, CURLOPT_URL, INVERTER_GET_INFO);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 60);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, receive_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK) {
+        fprintf(stderr, "fetch_inverter_info(): curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    } else {
+        strcpy(INVERTER_INFO_RES, "");
+        strcpy(INVERTER_INFO_RES, chunk.memory);
+    }
+
+    curl_easy_cleanup(curl);
+    free(chunk.memory);
+    curl_global_cleanup();
+    curl = NULL;
+}
+
+void fetch_inverter_status() {
+    CURL *curl = curl_easy_init();
+    CURLcode res;
+    struct memory_struct chunk;
+
+    chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */ 
+    chunk.size = 0;    /* no data at this point */ 
+
+    if (!curl) {
+        fprintf(stderr, "libcurl is not loaded correctly!\n");
+    }
+
+    // set cURL setting
+    curl_easy_setopt(curl, CURLOPT_URL, INVERTER_GET_STATUS);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 60);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, receive_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK) {
+        fprintf(stderr, "fetch_inverter_status(): curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    } else {
+        strcpy(INVERTER_STATUS_RES, "");
+        strcpy(INVERTER_STATUS_RES, chunk.memory);
+    }
+
+    curl_easy_cleanup(curl);
+    free(chunk.memory);
+    curl_global_cleanup();
+    curl = NULL;
+}
+
+void send_inverter_set(bool inverter_set_val) {
+    CURL *curl = curl_easy_init();
+    CURLcode res;
+    struct memory_struct chunk;
+
+    json_object *json;
+    enum json_tokener_error jerr = json_tokener_success;
+
+    struct curl_slist *headers = NULL;
+    bool success = true;
+
+    chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */ 
+    chunk.size = 0;    /* no data at this point */ 
+
+    if (!curl) {
+        fprintf(stderr, "libcurl is not loaded correctly!\n");
+    }
+
+    headers = curl_slist_append(headers, "Accept: application/json");
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+
+    json = json_object_new_object();
+    json_object_object_add(json, "InverterOn", json_object_new_boolean(inverter_set_val));
+    json_object_object_add(json, "note", json_object_new_string(""));
+    json_object_object_add(json, "success", json_object_new_boolean(success));
+
+
+    // set cURL setting
+    curl_easy_setopt(curl, CURLOPT_URL, INVERTER_SET);
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_object_to_json_string(json));
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 60);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, receive_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK) {
+        fprintf(stderr, "send_inverter_set(): curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    } else {
+        strcpy(INVERTER_SET_RES, "");
+        strcpy(INVERTER_SET_RES, chunk.memory);
+    }
+
+    curl_easy_cleanup(curl);
+    free(chunk.memory);
+    curl_global_cleanup();
+    curl_slist_free_all(headers);
+    curl = NULL;
+
+    json_object_put(json);
+}
+
+void read_config_file() {
+    strcpy(INVERTER_GET_INFO, "");
+    strcpy(INVERTER_GET_STATUS, "");
+    strcpy(INVERTER_SET, "");
+
+    char config_path[1000] = "/home/iec61850/config";
+    FILE *fp;
+    char buffer1[250], buffer2[250];
+
+    fp = fopen(config_path, "rb");
+    if (!fp) {
+        fprintf(stderr, "Cannot open %s file\n", config_path);
+        exit(1);
+    }
+
+    if(fgets(buffer1, sizeof(buffer1), fp)) {
+        if(1 == sscanf(buffer1, "inverter_on_off_api=%s", buffer2)) {
+            strcpy(INVERTER_SET, buffer2);
+        }
+    }
+
+    if(fgets(buffer1, sizeof(buffer1), fp)) {
+        if(1 == sscanf(buffer1, "inverter_status_api=%s", buffer2)) {
+            strcpy(INVERTER_GET_STATUS, buffer2);
+        }
+    }
+
+    if(fgets(buffer1, sizeof(buffer1), fp)) {
+        if(1 == sscanf(buffer1, "inverter_info_api=%s", buffer2)) {
+            strcpy(INVERTER_GET_INFO, buffer2);
+        }
+    }
+
+    fclose(fp);
+}
 
 void
 sigint_handler(int signalId)
@@ -31,6 +297,27 @@ writeAccessHandler (DataAttribute* dataAttribute, MmsValue* value, ClientConnect
     if (dataAttribute == IEDMODEL_GenericIO_GGIO1_NamPlt_vendor) {
         const char* newValue = MmsValue_toString(value);
         printf("New value for OutVarSet_setMag_f = %s\n", newValue);
+        fflush(stdout);
+        int compared_result = strcmp(newValue, INVERTER_ON_STRING);
+        if (compared_result == 0) {
+            printf("Inverter will send on operation\n");
+            fflush(stdout);
+
+            send_inverter_set(true);
+            process_response_json(INVERTER_SET_RES);
+            printf("Inverter getInfo API INVERTER_ON_OFF: %d\n", INVERTER_ON_OFF);
+            fflush(stdout);
+        }
+        compared_result = strcmp(newValue, INVERTER_OFF_STRING);
+        if (compared_result == 0) {
+            printf("Inverter will send off operation\n");
+            fflush(stdout);
+
+            send_inverter_set(false);
+            process_response_json(INVERTER_SET_RES);
+            printf("Inverter getInfo API INVERTER_ON_OFF: %d\n", INVERTER_ON_OFF);
+            fflush(stdout);
+        }
         return DATA_ACCESS_ERROR_SUCCESS;
     }
 
@@ -45,11 +332,16 @@ controlHandlerForBinaryOutput(ControlAction action, void* parameter, MmsValue* v
 
     if (MmsValue_getType(value) == MMS_BOOLEAN) {
         printf("received binary control command: ");
+        fflush(stdout);
 
-        if (MmsValue_getBoolean(value))
+        if (MmsValue_getBoolean(value)) {
             printf("on\n");
-        else
+            fflush(stdout);
+        }
+        else {
             printf("off\n");
+            fflush(stdout);
+        }
     }
     else
         return CONTROL_RESULT_FAILED;
@@ -82,10 +374,14 @@ controlHandlerForBinaryOutput(ControlAction action, void* parameter, MmsValue* v
 static void
 connectionHandler (IedServer self, ClientConnection connection, bool connected, void* parameter)
 {
-    if (connected)
+    if (connected) {
         printf("Connection opened\n");
-    else
+        fflush(stdout);
+    }
+    else {
         printf("Connection closed\n");
+        fflush(stdout);
+    }
 }
 
 static void
@@ -95,9 +391,12 @@ printAppTitle(ItuObjectIdentifier* oid)
 
     for (i = 0; i < oid->arcCount; i++) {
         printf("%i", oid->arc[i]);
+        fflush(stdout);
 
-        if (i != (oid->arcCount - 1))
+        if (i != (oid->arcCount - 1)) {
             printf(".");
+            fflush(stdout);
+        }
     }
 }
 
@@ -105,9 +404,16 @@ static bool
 clientAuthenticator(void* parameter, AcseAuthenticationParameter authParameter, void** securityToken, IsoApplicationReference* appRef)
 {
     printf("ACSE Authenticator:\n");
+    fflush(stdout);
+
     printf("  client ap-title: "); printAppTitle(&(appRef->apTitle)); printf("\n");
+    fflush(stdout);
+
     printf("  client ae-qualifier: %i\n", appRef->aeQualifier);
+    fflush(stdout);
+
     printf("  auth-mechanism: %i\n", authParameter->mechanism);
+    fflush(stdout);
 
     return true;
 }
@@ -115,12 +421,44 @@ clientAuthenticator(void* parameter, AcseAuthenticationParameter authParameter, 
 int
 main(int argc, char** argv)
 {
-    int port_number = 8102;
+    printf("Reading config file...\n");
+    fflush(stdout);
+    read_config_file();
+
+    printf("Read config file: first line is: %s\n", INVERTER_SET);
+    fflush(stdout);
+
+    printf("Read config file: second line is: %s\n", INVERTER_GET_STATUS);
+    fflush(stdout);
+
+    printf("Read config file: third line is: %s\n", INVERTER_GET_INFO);
+    fflush(stdout);
+
+    fetch_inverter_info();
+    process_response_json(INVERTER_INFO_RES);
+
+    printf("Inverter getInfo API INVERTER_ON_OFF: %d\n", INVERTER_ON_OFF);
+    fflush(stdout);
+
+    printf("Inverter getInfo API INVERTER_CONNECTION: %d\n", INVERTER_CONNECTION);
+    fflush(stdout);
+
+    printf("Inverter getInfo API INVERTER_VERSION: %s\n", INVERTER_VERSION);
+    fflush(stdout);
+
+    printf("Inverter getInfo API INVERTER_CONNECTION_STATUS: %s\n", INVERTER_CONNECTION_STATUS);
+    fflush(stdout);
+
+
+    int port_number = 8103;
     if (argc > 1)
         port_number = atoi(argv[1]);
 
     printf("Using libIEC61850 version %s\n", LibIEC61850_getVersionString());
+    fflush(stdout);
+
     printf("libIEC61850 IedServer server will listen on %d\n", port_number);
+    fflush(stdout);
 
     TLSConfiguration tlsConfig = TLSConfiguration_create();
 
@@ -129,16 +467,19 @@ main(int argc, char** argv)
 
     if (!TLSConfiguration_setOwnKeyFromFile(tlsConfig, "server-key.pem", NULL)) {
         printf("Failed to load private key!\n");
+        fflush(stdout);
         return 0;
     }
 
     if (!TLSConfiguration_setOwnCertificateFromFile(tlsConfig, "server.cer")) {
         printf("ERROR: Failed to load own certificate!\n");
+        fflush(stdout);
         return 0;
     }
 
     if (!TLSConfiguration_addCACertificateFromFile(tlsConfig, "root.cer")) {
         printf("ERROR: Failed to load root certificate\n");
+        fflush(stdout);
         return 0;
     }
 
@@ -148,16 +489,18 @@ main(int argc, char** argv)
 
     if (!TLSConfiguration_addAllowedCertificateFromFile(tlsConfig, "client1.cer")) {
         printf("ERROR: Failed to load allowed client certificate\n");
+        fflush(stdout);
         return 0;
     }
 
     if (!TLSConfiguration_addAllowedCertificateFromFile(tlsConfig, "client2.cer")) {
         printf("ERROR: Failed to load allowed client certificate\n");
+        fflush(stdout);
         return 0;
     }
 
     // Create Model IED
-    IedModel* myModel = IedModel_create("testmodel");
+    IedModel* myModel = IedModel_create("inverterModel");
 
     // Create Logical Device
     LogicalDevice* lDevice1 = LogicalDevice_create("SENSORS", myModel);
@@ -190,7 +533,7 @@ main(int argc, char** argv)
     IedServer_start(iedServer, port_number);
 
     if (!IedServer_isRunning(iedServer)) {
-        printf("Starting server failed! Exit.\n");
+        fprintf(stderr, "Starting server failed! Exit.\n");
         IedServer_destroy(iedServer);
         exit(-1);
     }
@@ -206,9 +549,12 @@ main(int argc, char** argv)
 
         t += 0.1f;
 
-        float an1 = 0.0001;
-        float an2 = sinf(t + 1.f);
-        float an3 = sinf(t + 2.f);
+        fetch_inverter_status();
+        process_response_json(INVERTER_STATUS_RES);
+
+        float watt = INVERTER_CURRENT;
+        float volt = INVERTER_VOLT;
+        float electric = INVERTER_POWER;
         float an4 = sinf(t + 3.f);
 
         IedServer_lockDataModel(iedServer);
@@ -224,13 +570,13 @@ main(int argc, char** argv)
             Timestamp_setClockNotSynchronized(&iecTimestamp, true);
 
         IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn1_t, &iecTimestamp);
-        IedServer_updateFloatAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn1_mag_f, an1);
+        IedServer_updateFloatAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn1_mag_f, watt);
 
         IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn2_t, &iecTimestamp);
-        IedServer_updateFloatAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn2_mag_f, an2);
+        IedServer_updateFloatAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn2_mag_f, volt);
 
         IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn3_t, &iecTimestamp);
-        IedServer_updateFloatAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn3_mag_f, an3);
+        IedServer_updateFloatAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn3_mag_f, electric);
 
         IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn4_t, &iecTimestamp);
         IedServer_updateFloatAttributeValue(iedServer, IEDMODEL_GenericIO_GGIO1_AnIn4_mag_f, an4);
